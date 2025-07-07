c65bafb3cd2ac0cf5353906838f5c0103ad42bce
NilofarPinjari <164488453+NilofarPinjari@users.noreply.github.com>
2025-07-07 17:35:31 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 import './Header.css';
-import { FaSearch } from "react-icons/fa";
-import { IoMdNotifications } from "react-icons/io";
-import { IoSettings } from "react-icons/io5";
+import {FaSearch} from "react-icons/fa";
+import {IoMdNotifications} from "react-icons/io";
+import {IoSettings} from "react-icons/io5";
 const Header = () => (
   <header className="header">
 
-    <h2>Dashboard</h2>
+  <h2>Dashboard</h2>
   
-    <div className="head">
-      <span className="icon"><FaSearch />
-</span>
+      <div className="head">
+      <span className="icon"><FaSearch /></span>
       <span className="icon"><IoMdNotifications /></span>
       <span className="icon"><IoSettings /></span>
       <img src="/avatar.jpg" alt="profile" className="avatar" />
